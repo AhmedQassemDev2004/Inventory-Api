eac82531a890717786920790d7c5c126d96e57e0
ahmedqassem <ahmedqasem043@gmail.com>
2023-11-27 00:17:10 +0200

    edit main.ts

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('documentation', app, document);
 
   await app.listen(process.env.PORT || 8000);
 }
